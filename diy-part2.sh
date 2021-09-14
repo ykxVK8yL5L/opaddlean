c6fca9d15334c72c6e36dd780d86d38a14f1a795
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-09-14 09:55:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.168/g' package/base-files/files/bin/config_generate
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
-sed -i 's/set root=\'\(hd0,gpt1\)\'/set root=\'\(hd1,gpt1\)\'/g' target/linux/x86/image/grub-efi.cfg
+sed -i 's/hd0,gpt1/hd1,gpt1/g' target/linux/x86/image/grub-efi.cfg
