e5c7a1fa15167ffa175193aabd08e2d3fe7497eb
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2022-09-30 09:38:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@ sed -i 's/192.168.1.1/192.168.1.168/g' package/base-files/files/bin/config_gener
 #sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 #sed -i 's/hd0,gpt1/hd1,gpt1/g' target/linux/x86/image/grub-efi.cfg
 # 修改默认密码为root
-#sed -i "s|root::0:0:99999:7:::|root:$1$8pz6InR5$akb\/zWDtsSfL2ZUpm0ep70:19241:0:99999:7:::|g" package/base-files/files/etc/shadow
+sed -i "s|root::0:0:99999:7:::|root:$1$8pz6InR5$akb\/zWDtsSfL2ZUpm0ep70:19241:0:99999:7:::|g" package/base-files/files/etc/shadow
 
 # 一些包冲突
 #rm -rf feeds/kenzo/filebrowser
