55936c28c8396f1f752614e0ed542d92056ea650
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2022-10-03 23:31:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,8 +16,8 @@ sed -i 's/192.168.1.1/192.168.1.168/g' package/base-files/files/bin/config_gener
 # 删除一些不需要的lean配置【默认软件源和默认密码】
 sed -i '33d;34d' package/lean/default-settings/files/zzz-default-settings
 # 修改默认密码为root【需要删除lean的配置文件里的设置或直接修改配置文件】
-sed -i 's/root::0:0:99999:7:::/root:$1$rq6132gg$G0rIIMm.BvYx9Fm8b4ES4\/:0:0:99999:7:::/g' package/base-files/files/etc/shadow
-#sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$rq6132gg$G0rIIMm.BvYx9Fm8b4ES4\/:0:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/root::0:0:99999:7:::/root:$1$rq6132gg$G0rIIMm.BvYx9Fm8b4ES4\/:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$rq6132gg$G0rIIMm.BvYx9Fm8b4ES4\/:0:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 
 # 添加openwrt.cc软件源
 sed -i '$i echo "src/gz openwrt_cc_base https://openwrt.cc/snapshots/packages/x86_64/base\nsrc/gz openwrt_cc_luci https://openwrt.cc/snapshots/packages/x86_64/luci\nsrc/gz openwrt_cc_packages https://openwrt.cc/snapshots/packages/x86_64/packages\nsrc/gz openwrt_cc_routing https://openwrt.cc/snapshots/packages/x86_64/routing\nsrc/gz openwrt_cc_telephony https://openwrt.cc/snapshots/packages/x86_64/telephony">>/etc/opkg/customfeeds.conf' package/lean/default-settings/files/zzz-default-settings
