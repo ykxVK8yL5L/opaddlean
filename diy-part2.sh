8739d43894969238d7a430b1665b4b937051b206
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2022-10-03 09:02:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,11 @@ sed -i 's/192.168.1.1/192.168.1.168/g' package/base-files/files/bin/config_gener
 #sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 #sed -i 's/hd0,gpt1/hd1,gpt1/g' target/linux/x86/image/grub-efi.cfg
 # 修改默认密码为root【失败，默认密码不是空】
-#sed -i "s|root::0:0:99999:7:::|root:$1$8pz6InR5$akb\/zWDtsSfL2ZUpm0ep70:19241:0:99999:7:::|g" package/base-files/files/etc/shadow
-sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$8pz6InR5$akb\/zWDtsSfL2ZUpm0ep70:19241:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+sed -i "s|root::0:0:99999:7:::|root:$1$8pz6InR5$akb\/zWDtsSfL2ZUpm0ep70:19241:0:99999:7:::|g" package/base-files/files/etc/shadow
+#sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$8pz6InR5$akb\/zWDtsSfL2ZUpm0ep70:19241:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+
+# 删除一些不需要的lean配置
+sed -i '34d;37d' package/lean/default-settings/files/zzz-default-settings
 
 
 
