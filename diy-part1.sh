dd78d53e356f4e76efa70a1ff1bbfca9d9ca4dd0
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2022-10-31 08:27:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,7 +24,7 @@
 svn co https://github.com/ykxVK8yL5L/pikpak-webdav/trunk/openwrt package/luci-app-pikpak-webdav
 #Axum-web
 svn co https://github.com/ykxVK8yL5L/axum-web/trunk/openwrt package/luci-app-axum-web
-
+svn co https://github.com/ykxVK8yL5L/luci-app-taskschedule/trunk/luci-app-taskschedule package/luci-app-taskschedule
 
 
 #Alist
